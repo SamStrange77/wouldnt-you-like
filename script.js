522f4407d56a369652e9e8ac71611b5c21947d5a
SamStrange7 <63666828+SamStrange77@users.noreply.github.com>
2025-05-20 18:19:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let initialDate = new Date("2025-05-20").setHours(0,0,0,0);
 let TODAY = new Date().toISOString().split("T")[0];
 let songListModes = [0, 2];
 
+console.log('VERSION: ' + 1);
+console.log('CHANGES: CHANGED LOCAL STORAGE');
 console.log(TODAY + ': ' + dailyRandom(seedify(12)));
 
 let songList = 
